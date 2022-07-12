1963ef9df88d792be956551fdf23d7df02aefba3
Peter Kwan <peterkwkwan@gmail.com>
2022-07-12 13:55:24 +0800

    feat: config

--- config.js
+++ config.js
@@ -5,6 +5,15 @@ const config = {
     password: "290864ff",
     database: "heroku_bcb01a1941a0366",
   },
+
+  // --- For running app locally --- //
+  // db: {
+  //   host: "localhost",
+  //   user: "newuser",
+  //   password: "password",
+  //   database: "defendjeditemple",
+  // },
+
   listPerPage: 10,
 };
 
